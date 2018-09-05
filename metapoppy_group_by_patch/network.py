151e58f69724f4795713746a29554bb18412b659
michaeljpitcher <mjp22@st-andrews.ac.uk>
2018-09-05 09:33:32 +0100

    Comments

--- metapoppy_group_by_patch/network.py
+++ metapoppy_group_by_patch/network.py
@@ -3,6 +3,9 @@ import numpy
 
 
 class Network(networkx.Graph):
+    """
+    A MetapopPy network. Extends networkX graph, adding data for patch subpopulations and environmental attributes.
+    """
 
     INITIAL = 'initial'
     COMPARTMENTS = 'compartments'
@@ -11,21 +14,45 @@ class Network(networkx.Graph):
     TOTAL_RATE = 'total_rate'
 
     def __init__(self, compartments, patch_attributes, edge_attributes):
+        """
+        Create a network
+        :param compartments: List of population compartments
+        :param patch_attributes: List of patch attributes
+        :param edge_attributes: List of edge attributes
+        """
         self._compartments = compartments
         self._patch_attributes = patch_attributes
         self._edge_attributes = edge_attributes
         networkx.Graph.__init__(self)
 
     def compartments(self):
+        """
+        Get function for compartments
+        :return:
+        """
         return self._compartments
 
     def patch_attributes(self):
+        """
+        Get function for patch attributes
+        :return:
+        """
         return self._patch_attributes
 
     def edge_attributes(self):
+        """
+        Get function for edge attributes
+        :return:
+        """
         return self._edge_attributes
 
     def prepare(self):
+        """
+        Prepare the network. Adds data to the node dictionary, for subpopulation, environmental attributes and events
+        and rates. All set to zero, will be seeded with a value once simulation runs.
+        :return:
+        """
+        # Prepare the network
         for n in self.nodes():
             self.node[n][Network.COMPARTMENTS] = dict([(c, 0) for c in self._compartments])
             self.node[n][Network.ATTRIBUTES] = dict([(c, 0) for c in self._patch_attributes])
@@ -38,42 +65,55 @@ class Network(networkx.Graph):
     # TODO - update the add node/edge functions with prepare (as above) to allow for dynamic graphs
 
     def get_compartment_value(self, patch_id, compartment):
+        """
+        Get function for finding a compartment value at a patch
+        :param patch_id:
+        :param compartment:
+        :return:
+        """
         return self.node[patch_id][Network.COMPARTMENTS][compartment]
 
     def get_attribute_value(self, patch_id, attribute):
+        """
+        Get function for finding an environmental attribute value at a patch
+        :param patch_id:
+        :param attribute:
+        :return:
+        """
         return self.node[patch_id][Network.ATTRIBUTES][attribute]
 
     def attach_event(self, event, patch_id):
+        """
+        Attach the given event to the patch
+        :param event:
+        :param patch_id:
+        :return:
+        """
         event.set_patch_id(patch_id)
         self.node[patch_id][Network.EVENTS].append(event)
 
     def update_rates_at_patch(self, patch_id):
+        """
+        Loop through all events at a patch and trigger a rate update
+        :param patch_id:
+        :return:
+        """
+        # TODO - dependencies to improve efficiency here
         patch_data = self.node[patch_id]
-        for event in self.node[patch_id][Network.EVENTS]:
+        for event in patch_data[Network.EVENTS]:
             rate_before = event.rate()
             event.update_rate(patch_data, [d for _,_,d in self.edges([patch_id], data=True)])
             rate_difference = event.rate() - rate_before
             patch_data[Network.TOTAL_RATE] += rate_difference
 
-    def perform_an_event(self):
-        total_network_rate = sum([d[Network.TOTAL_RATE] for _,d in self.nodes(data=True)])
-        r = numpy.random.random() * total_network_rate
-        count = 0
-        # Loop through all patches adding the total rate at each patch
-        for n,d in self.nodes(data=True):
-            # If the total rate at this patch would exceed the r number, this is where an event will occur
-            if count + d[Network.TOTAL_RATE] > r:
-                # Find the event which tips the count over r
-                for e in d[Network.EVENTS]:
-                    count += e.rate()
-                    if count >= r:
-                        # Perform the event and stop iterations
-                        e.perform(self)
-                        return
-            else:
-                count += d[Network.TOTAL_RATE]
-
     def update_patch(self, patch_id, compartment_changes=None, attribute_changes=None):
+        """
+        Update the values at a patch. Also triggers updates of the rates of events at the patch.
+        :param patch_id: Patch ID in the network
+        :param compartment_changes: Changes to compartments
+        :param attribute_changes: Changes to environmental attributes.
+        :return:
+        """
         patch_data = self.node[patch_id]
         if compartment_changes:
             for (c,v) in compartment_changes.iteritems():
@@ -84,6 +124,14 @@ class Network(networkx.Graph):
         self.update_rates_at_patch(patch_id)
 
     def update_edge(self, u, v, attribute_changes):
+        """
+        Update the environmental attributes of the edge. Also triggers rate update of the events at the connected
+        patches.
+        :param u: Patch in network
+        :param v: Patch in network
+        :param attribute_changes: Changes to attributes
+        :return:
+        """
         for att,value in attribute_changes.iteritems():
             self.edge[u][v][att] += value
         self.update_rates_at_patch(u)
