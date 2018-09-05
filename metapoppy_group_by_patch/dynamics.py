0e7615892fa4afec913d57d16f423ade7484ed6b
michaeljpitcher <mjp22@st-andrews.ac.uk>
2018-09-05 09:33:21 +0100

    Comments, results and initial time

--- metapoppy_group_by_patch/dynamics.py
+++ metapoppy_group_by_patch/dynamics.py
@@ -7,18 +7,27 @@ from network import *
 class Dynamics(epyc.Experiment, object):
     INITIAL_PATCHES = 'initial_patches'
     INITIAL_EDGES = 'initial_edges'
+    INITIAL_TIME = 'inital_time'
 
     # the default maximum simulation time
     DEFAULT_MAX_TIME = 20000  #: Default maximum simulation time.
+    DEFAULT_START_TIME = 0
 
     def __init__(self, g):
+        """
+        Create MetapopPy dynamics to run over the given network.
+        :param g:
+        """
+        assert isinstance(g, Network), "Graph must be instance of MetapopPy Network class"
         epyc.Experiment.__init__(self)
         self._compartments = g.compartments()
         self._patch_attributes = g.patch_attributes()
         self._edge_attributes = g.edge_attributes()
 
+        # Prepare the network
         self._graphPrototype = g
         self._graphPrototype.prepare()
+
         self._create_events(self._graphPrototype)
 
         self._graph = None
@@ -29,7 +38,7 @@ class Dynamics(epyc.Experiment, object):
 
     def network(self):
         """
-        The network this dynamics runs upon
+        The network this set of dynamics runs upon
         :return:
         """
         return self._graph
@@ -51,6 +60,11 @@ class Dynamics(epyc.Experiment, object):
         return t >= self._maxTime
 
     def setUp(self, params):
+        """
+        Configure the dynamics and the network ready for a simulation.
+        :param params: Initial parameters - initial conditions of network and event reaction parameters.
+        :return:
+        """
         # Default setup
         epyc.Experiment.setUp(self, params)
 
@@ -68,7 +82,12 @@ class Dynamics(epyc.Experiment, object):
         # Set the event reactions parameters using the initial conditions
         self._seed_events(params[Network.EVENTS])
 
-    def _seed_events(self, params):
+    def _seed_events(self, event_parameters):
+        """
+        Set the event reaction parameters
+        :param params:
+        :return:
+        """
         raise NotImplementedError
 
     def do(self, params):
@@ -79,21 +98,28 @@ class Dynamics(epyc.Experiment, object):
         :return:
         """
 
-        # run the dynamics
-        time = 0
+        # Allow for designated start time (used for time/age specific events)
+        if Dynamics.INITIAL_TIME in params:
+            time = params[Dynamics.INITIAL_TIME]
+        else:
+            time = Dynamics.DEFAULT_START_TIME
 
         while not self._at_equilibrium(time):
+            # TODO - this is inefficient, should post changes to dynamics
+            # Get the total rate by summing rates at all patches
             total_network_rate = sum([d[Network.TOTAL_RATE] for _, d in self._graph.nodes(data=True)])
 
+            # No events can occur, so end
             if total_network_rate == 0:
                 break
 
-            # calculate the timestep delta
+            # Calculate the timestep delta
             dt = (1.0 / total_network_rate) * math.log(1.0 / numpy.random.random())
 
             # Choose an event
             r = numpy.random.random() * total_network_rate
             count = 0
+
             # Loop through all patches adding the total rate at each patch
             for n, d in self._graph.nodes(data=True):
                 # If the total rate at this patch would exceed the r number, this is where an event will occur
@@ -104,18 +130,29 @@ class Dynamics(epyc.Experiment, object):
                         if count >= r:
                             # Perform the event and stop iterations
                             e.perform(self._graph)
-                            return
+                            # Stop looping through events
+                            break
+                    # Stop looping through patches
+                    break
+                # Have not exceeded threshold, so continue
                 else:
                     count += d[Network.TOTAL_RATE]
+            # Move simulated time forward
             time += dt
+
+        # TODO - experimental results. Maybe need to track populations over time
+        return self.get_results()
+
+    def get_results(self):
+        return None
 
     def tearDown(self):
         """
         After a simulation ends, destroy the used graph.
         :return:
         """
-        # perform the default tear-down
+        # Perform the default tear-down
         epyc.Experiment.tearDown(self)
 
-        # throw away the worked-on model
+        # Destroy the worked-on model
         self._graph = None
