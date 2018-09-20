e00c77abd43f3a551eb3e4870c82c1bc5e51bebd
michaeljpitcher <mjp22@st-andrews.ac.uk>
2018-09-20 12:14:27 +0100

    TCell Activation event

---
+++ tbmetapoppy/events/tcell_activation.py
@@ -0,0 +1,20 @@
+from tbmetapoppy import *
+from metapoppy.event import PatchTypeEvent
+
+
+class TCellActivation(PatchTypeEvent):
+    def __init__(self, antigen_presenter):
+        self._antigen_presenter = antigen_presenter
+        self._half_sat = 0
+        PatchTypeEvent.__init__(self, PulmonaryNetwork.LYMPH_PATCH)
+
+    def _calculate_state_variable_at_patch(self, network, patch_id):
+        apc_count = network.get_compartment_value(patch_id, self._antigen_presenter)
+        if not apc_count:
+            return 0
+        return network.get_compartment_value(patch_id, TBDynamics.T_CELL_NAIVE) * \
+               (apc_count / (apc_count + self._half_sat))
+
+    def perform(self, network, patch_id):
+        network.update_patch(patch_id, {TBDynamics.T_CELL_NAIVE: -1,
+                                        TBDynamics.T_CELL_ACTIVATED: 1})
