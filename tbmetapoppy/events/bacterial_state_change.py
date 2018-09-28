b05af548b805c92be61c8c2c461484958e66b454
michaeljpitcher <mjp22@st-andrews.ac.uk>
2018-09-28 13:14:17 +0100

    Handle 0 values

--- tbmetapoppy/events/bacterial_state_change.py
+++ tbmetapoppy/events/bacterial_state_change.py
@@ -18,9 +18,11 @@ class BacteriumBecomesDormant(PatchTypeEvent):
         self._half_sat = half_sat
 
     def _calculate_state_variable_at_patch(self, network, patch_id):
+        bac = network.get_compartment_value(patch_id, BACTERIUM_EXTRACELLULAR_REPLICATING)
+        if not bac:
+            return 0
         o2 = network.get_attribute_value(patch_id, PulmonaryNetwork.OXYGEN_TENSION)
-        return network.get_compartment_value(patch_id, BACTERIUM_EXTRACELLULAR_REPLICATING) * \
-            ((o2 ** self._sigmoid) / (self._half_sat ** self._sigmoid + o2 ** self._sigmoid))
+        return bac * ((o2 ** self._sigmoid) / (self._half_sat ** self._sigmoid + o2 ** self._sigmoid))
 
     def perform(self, network, patch_id):
         network.update_patch(patch_id, {BACTERIUM_EXTRACELLULAR_REPLICATING: -1, BACTERIUM_EXTRACELLULAR_DORMANT: 1})
@@ -39,9 +41,9 @@ class BacteriumBecomesReplicating(PatchTypeEvent):
         self._half_sat = half_sat
 
     def _calculate_state_variable_at_patch(self, network, patch_id):
+        bac = network.get_compartment_value(patch_id, BACTERIUM_EXTRACELLULAR_DORMANT)
         o2 = network.get_attribute_value(patch_id, PulmonaryNetwork.OXYGEN_TENSION)
-        return network.get_compartment_value(patch_id, BACTERIUM_EXTRACELLULAR_DORMANT) * \
-            ((o2 ** self._sigmoid) / (self._half_sat ** self._sigmoid + o2 ** self._sigmoid))
+        return bac * ((o2 ** self._sigmoid) / (self._half_sat ** self._sigmoid + o2 ** self._sigmoid))
 
     def perform(self, network, patch_id):
         network.update_patch(patch_id, {BACTERIUM_EXTRACELLULAR_DORMANT: -1, BACTERIUM_EXTRACELLULAR_REPLICATING: 1})
