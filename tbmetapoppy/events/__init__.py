c35bba65168e4e4a6193c0f4dba05d44cf72f6b8
michaeljpitcher <mjp22@st-andrews.ac.uk>
2019-02-05 13:53:10 +0000

    New cell translocation

--- tbmetapoppy/events/__init__.py
+++ tbmetapoppy/events/__init__.py
@@ -3,6 +3,6 @@ from bacterial_state_change import *
 from cell_death import *
 from cell_ingestion import *
 from cell_recruitment import *
-from cell_translocation import *
 from cell_activation import *
 from parameters import *
+from cell_translocation import *
--- tbmetapoppy/events/cell_translocation.py
+++ tbmetapoppy/events/cell_translocation.py
@@ -1,91 +1,95 @@
 from tbmetapoppy.tbpulmonarynetwork import TBPulmonaryNetwork
 from metapoppy.event import PatchTypeEvent
-from parameters import RATE, HALF_SAT
+from parameters import RATE, SIGMOID
 import numpy
 
 
-class CellTranslocationToLymph(PatchTypeEvent):
-    TRANSLOCATION_TO_LYMPH = '_translocation_to_lymph' + RATE
+class Translocation(PatchTypeEvent):
 
-    def __init__(self, cell_type):
+    TRANSLOCATION_KEY = '_translocation_from_'
+
+    def __init__(self, patch_type, cell_type):
         self._cell_type = cell_type
+        PatchTypeEvent.__init__(self, patch_type, [cell_type], [], [])
         if cell_type in TBPulmonaryNetwork.INTERNAL_BACTERIA_FOR_CELL:
             self._internal_compartment = TBPulmonaryNetwork.INTERNAL_BACTERIA_FOR_CELL[cell_type]
         else:
             self._internal_compartment = None
-        PatchTypeEvent.__init__(self, TBPulmonaryNetwork.ALVEOLAR_PATCH, [cell_type], [TBPulmonaryNetwork.DRAINAGE], [])
 
     def _define_parameter_keys(self):
-        rp_key = self._cell_type + CellTranslocationToLymph.TRANSLOCATION_TO_LYMPH
-        return rp_key, []
-
-    def _calculate_state_variable_at_patch(self, network, patch_id):
-        return network.get_compartment_value(patch_id, self._cell_type) * \
-               network.get_attribute_value(patch_id, TBPulmonaryNetwork.DRAINAGE)
-
-    def perform(self, network, patch_id):
-        changes_lung = {self._cell_type: -1}
-        changes_lymph = {self._cell_type: 1}
-        # TODO - assumes only one lymph patch with PulmonaryNetwork.LYMPH_PATCH as patch ID
-        lymph_id = TBPulmonaryNetwork.LYMPH_PATCH
-        if self._internal_compartment:
-            internals_to_transfer = int(round(float(
-                                     network.get_compartment_value(patch_id, self._internal_compartment)) /
-                                     network.get_compartment_value(patch_id, self._cell_type)))
-            changes_lung[self._internal_compartment] = -1 * internals_to_transfer
-            changes_lymph[self._internal_compartment] = internals_to_transfer
-        network.update_patch(patch_id, changes_lung)
-        network.update_patch(lymph_id, changes_lymph)
-
-
-class CellTranslocationToLung(PatchTypeEvent):
-    TRANSLOCATION_TO_LUNG = '_translocation_to_lung' + RATE
-
-    def __init__(self, cell_type):
-        self._cell_type = cell_type
-        PatchTypeEvent.__init__(self, TBPulmonaryNetwork.LYMPH_PATCH, [cell_type], [], [])
-
-    def _define_parameter_keys(self):
-        rp_key = self._cell_type + CellTranslocationToLung.TRANSLOCATION_TO_LUNG
-        return rp_key, []
+        return self._cell_type + Translocation.TRANSLOCATION_KEY + self._patch_type + RATE, []
 
     def _calculate_state_variable_at_patch(self, network, patch_id):
         return network.get_compartment_value(patch_id, self._cell_type)
 
     def perform(self, network, patch_id):
-        edges = network.edges([patch_id],data=True)
-        perfusions = [d[TBPulmonaryNetwork.PERFUSION] for _, _, d in edges]
-        total_perfusion = sum(perfusions)
+        neighbour = self._choose_neighbour(network, patch_id)
+        self._translocate(network, patch_id, neighbour)
 
-        lung_patch_id = numpy.random.choice([n for _,n,_ in edges], p=numpy.array(perfusions)/total_perfusion)
-        network.update_patch(patch_id, {self._cell_type: -1})
-        network.update_patch(lung_patch_id, {self._cell_type: 1})
+    def _choose_neighbour(self, network, patch_id):
+        edges = network[patch_id]
+        if len(edges) == 1:
+            return edges.keys()[0]
+        else:
+            return numpy.random.choice(edges.keys())
+
+    def _translocate(self, network, patch_id, neighbour):
+        changes_from = {self._cell_type: -1}
+        changes_to = {self._cell_type: 1}
+        if self._internal_compartment:
+            internals_to_transfer = int(round(float(
+                network.get_compartment_value(patch_id, self._internal_compartment)) /
+                network.get_compartment_value(patch_id, self._cell_type)))
+            changes_from[self._internal_compartment] = -1 * internals_to_transfer
+            changes_to[self._internal_compartment] = internals_to_transfer
+        network.update_patch(patch_id, changes_from)
+        network.update_patch(neighbour, changes_to)
 
 
-class TCellTranslocationToLungByInfection(CellTranslocationToLung):
-    def __init__(self):
-        CellTranslocationToLung.__init__(self, TBPulmonaryNetwork.T_CELL_ACTIVATED)
+class TranslocationLungToLymph(Translocation):
+    def __init__(self, cell_type):
+        Translocation.__init__(self, TBPulmonaryNetwork.ALVEOLAR_PATCH, cell_type)
 
-    # TODO - this forces t-cells into the lung even if infection has been wiped out. State var maybe should depend
-    #  on infection levels
+    def _calculate_state_variable_at_patch(self, network, patch_id):
+        return network.get_compartment_value(patch_id, self._cell_type) * \
+               network.get_attribute_value(patch_id, TBPulmonaryNetwork.DRAINAGE)
 
-    def perform(self, network, patch_id):
+
+class TranslocationLymphToLung(Translocation):
+    def __init__(self, cell_type):
+        Translocation.__init__(self, TBPulmonaryNetwork.LYMPH_PATCH, cell_type)
+
+    def _define_parameter_keys(self):
+        self._sigmoid_key = self._cell_type + Translocation.TRANSLOCATION_KEY + self._patch_type + SIGMOID
+        return self._cell_type + Translocation.TRANSLOCATION_KEY + self._patch_type + RATE, [self._sigmoid_key]
+
+    def _calculate_state_variable_at_patch(self, network, patch_id):
+        cells = network.get_compartment_value(patch_id, self._cell_type)
+        if not cells:
+            return 0
         infected_patches = network.infected_patches()
-        if len(infected_patches) == 1:
-            network.update_patch(patch_id, {self._cell_type: -1})
-            network.update_patch(infected_patches[0], {self._cell_type: 1})
-            return
+        cytokine_count_lung = sum([network.get_edge_data(patch_id, n)[TBPulmonaryNetwork.CYTOKINE] for n in infected_patches])
+        cytokine_count_lymph = network.get_compartment_value(patch_id, TBPulmonaryNetwork.MACROPHAGE_INFECTED)
+        # Catch to avoid / 0 errors
+        if not cytokine_count_lymph and not cytokine_count_lung:
+            return 0
+        return network.get_compartment_value(patch_id, TBPulmonaryNetwork.T_CELL_ACTIVATED) * \
+               (float(cytokine_count_lung) ** self._parameters[self._sigmoid_key] /
+                (cytokine_count_lung ** self._parameters[self._sigmoid_key] +
+                 cytokine_count_lymph ** self._parameters[self._sigmoid_key]))
 
-        infections = [network.get_attribute_value(n, TBPulmonaryNetwork.PERFUSION) *
-                      network.get_compartment_value(n, TBPulmonaryNetwork.EXTRACELLULAR_BACTERIA)
-                      for n in infected_patches]
-        total_infection = sum(infections)
+    def _choose_neighbour(self, network, patch_id):
+        # Choosing based on infection and perfusion
+        infected_patches = network.infected_patches()
+        edges = {n: network.get_edge_data(patch_id, n) for n in infected_patches}
+        neighbours = []
+        vals = []
+        total = 0
+        for k,v in edges.iteritems():
+            neighbours.append(k)
+            val = v[TBPulmonaryNetwork.CYTOKINE] * v[TBPulmonaryNetwork.PERFUSION]
+            vals.append(val)
+            total += val
+        # Choose a neighbour based on the values
+        return neighbours[numpy.random.choice(range(len(neighbours)), p=numpy.array(vals)/total)]
 
-        if not total_infection:
-            return CellTranslocationToLung.perform(self, network, patch_id)
-
-        lung_patch_id = numpy.random.choice(infected_patches, p=numpy.array(infections)/total_infection)
-        network.update_patch(patch_id, {self._cell_type: -1})
-        network.update_patch(lung_patch_id, {self._cell_type: 1})
-
-# TODO translocation within lung
