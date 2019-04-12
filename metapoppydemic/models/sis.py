9254ec175bd34b2ce109b5373230f1e6edb5daf2
michaeljpitcher <mjp22@st-andrews.ac.uk>
2019-04-12 11:41:38 +0100

    Reaction parameter key fixed

--- metapoppydemic/models/sis.py
+++ metapoppydemic/models/sis.py
@@ -6,22 +6,22 @@ from compartments import *
 
 class SISDynamics(Epidemic):
 
-    RP_INFECT = 'infection_rate'
-    RP_RECOVER = 'recover_rate'
-    RP_MOVE_S = 's_move_rate'
-    RP_MOVE_I = 'i_move_rate'
-
     INIT_S = 'initial_population_susceptible'
     INIT_I = 'initial_population_infected'
 
     def __init__(self, template_network):
+        self.rp_infect_key = self.rp_recover_key = self.rp_move_s_key = self.rp_move_i_key = None
         Epidemic.__init__(self, [SUSCEPTIBLE, INFECTIOUS], template_network)
 
     def _create_events(self):
         infect = Infect(SUSCEPTIBLE, INFECTIOUS, INFECTIOUS)
+        self.rp_infect_key = infect.reaction_parameter()
         recover = Change(INFECTIOUS, SUSCEPTIBLE)
+        self.rp_recover_key = recover.reaction_parameter()
         move_s = Move(SUSCEPTIBLE)
+        self.rp_move_s_key = move_s.reaction_parameter()
         move_i = Move(INFECTIOUS)
+        self.rp_move_i_key = move_i.reaction_parameter()
         return [infect, recover, move_s, move_i]
 
     def _build_network(self, params):
