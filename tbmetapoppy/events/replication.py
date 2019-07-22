e7eb3aae25b229cccc178c1194e76cf57424ffce
michaeljpitcher <mjp22@st-andrews.ac.uk>
2019-07-22 13:29:58 +0100

    Non-specific replication

--- tbmetapoppy/events/replication.py
+++ tbmetapoppy/events/replication.py
@@ -1,4 +1,4 @@
-from metapoppy.event import Event
+from metapoppy.event import *
 from ..tbpulmonaryenvironment import *
 from parameters import *
 
@@ -41,3 +41,29 @@ class IntracellularBacterialReplication(Replication):
         cap = self._parameters[MACROPHAGE_CAPACITY]
         mac = network.get_compartment_value(patch_id, TBPulmonaryEnvironment.MACROPHAGE_INFECTED)
         return bac * (1 - (float(bac ** sig) / (bac ** sig + (cap * mac) ** sig)))
+
+
+class PatchTypeReplication(PatchTypeEvent):
+
+    REPLICATION_RATE = '_replication_rate'
+    CAPACITY = '_replication_capacity'
+    SIGMOID = '_replication_sigmoid'
+
+    def __init__(self, patch_type, cell_type):
+        self._cell_type = cell_type
+        PatchTypeEvent.__init__(self, patch_type, [self._cell_type], [], [])
+
+    def _define_parameter_keys(self):
+        self.cap_key = self._cell_type + '_' + self._patch_type + PatchTypeReplication.CAPACITY
+        self.sig_key = self._cell_type + '_' + self._patch_type + PatchTypeReplication.SIGMOID
+        return self._cell_type + '_' + self._patch_type + PatchTypeReplication.REPLICATION_RATE, [self.cap_key, self.sig_key]
+
+    def _calculate_state_variable_at_patch(self, network, patch_id):
+        cell = network.get_compartment_value(patch_id, self._cell_type)
+        if not cell:
+            return 0
+        sig = self._parameters[self.sig_key]
+        return cell * (1 - float(cell)**sig / (cell**sig + self._parameters[self.cap_key]**sig))
+
+    def perform(self, network, patch_id):
+        network.update_patch(patch_id, {self._cell_type: 1})
